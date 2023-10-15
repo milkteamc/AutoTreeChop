8c1d8a5918f879c49407c4aa920b337ad1d12073
Maoyue <95519633+MagicTeaMC@users.noreply.github.com>
2023-10-15 08:27:21 +0800

    limit max block break and better config file

--- src/main/java/org/milkteamc/autotreechop/AutoTreeChop.java
+++ src/main/java/org/milkteamc/autotreechop/AutoTreeChop.java
@@ -15,7 +15,9 @@ import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.util.*;
@@ -27,7 +29,9 @@ public class AutoTreeChop extends JavaPlugin implements Listener, CommandExecuto
     private String disabledMessage;
     private String noPermissionMessage;
     private String hitmaxusageMessage;
+    private String hitmaxblockMessage;
     private int maxUsesPerDay;
+    private int maxBlocksPerDay;
 
     @Override
     public void onEnable() {
@@ -42,20 +46,58 @@ public class AutoTreeChop extends JavaPlugin implements Listener, CommandExecuto
     }
 
     private void loadConfig() {
-        getConfig().addDefault("messages.enabled", "§a已開啟自動砍樹。");
-        getConfig().addDefault("messages.disabled", "§c已關閉自動砍樹。");
-        getConfig().addDefault("messages.no-permission", "§c你沒有權限使用此指令。");
-        getConfig().addDefault("messages.hitmaxusage", "§c你已達到每日使用次數限制。");
-        getConfig().addDefault("max-uses-per-day", 50);
-        getConfig().options().copyDefaults(true);
-        saveConfig();
+        File configFile = new File(getDataFolder(), "config.yml");
 
-        FileConfiguration config = getConfig();
+        if (!configFile.exists()) {
+            try {
+                if (!configFile.getParentFile().exists()) {
+                    configFile.getParentFile().mkdirs();
+                }
+                configFile.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+                return; // If there's an error creating the file, exit the method.
+            }
+        }
+
+        Map<String, String> configWithComments = new LinkedHashMap<>();
+        configWithComments.put("# AutoTreeChop by Maoyue", "");
+        configWithComments.put("# Discord support server: https://discord.gg/uQ4UXANnP2", "");
+        configWithComments.put("# Spigot Page: https://www.spigotmc.org/resources/113071", "");
+        configWithComments.put("", ""); // Empty line
+        configWithComments.put("# The number of times non-VIP players can chop down trees per day,", "");
+        configWithComments.put("# you can give everyone \"autotreechop.vip\" permission to disable it.", "");
+        configWithComments.put("max-uses-per-day", "50");
+        configWithComments.put("# Tree blocks that non-VIP players can chop down every day,", "");
+        configWithComments.put("# you can give everyone \"autotreechop.vip\" permission to disable it.", "");
+        configWithComments.put("max-blocks-per-day", "500");
+        configWithComments.put("", ""); // Empty line
+        configWithComments.put("# Plugin message", "");
+        configWithComments.put("messages:", "");
+        configWithComments.put("  enabled", "§aAuto tree chopping enabled.");
+        configWithComments.put("  disabled", "§cAuto tree chopping disabled.");
+        configWithComments.put("  no-permission", "§cYou don't have permission to use this command.");
+        configWithComments.put("  hitmaxusage", "§cYou've reached the daily usage limit.");
+        configWithComments.put("  hitmaxblock", "§cYou have reached your daily block breaking limit.");
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(configFile))) {
+            for (Map.Entry<String, String> entry : configWithComments.entrySet()) {
+                writer.write(entry.getKey() + (entry.getValue().isEmpty() ? "" : ": " + entry.getValue()));
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        FileConfiguration config = YamlConfiguration.loadConfiguration(configFile);
+
         enabledMessage = config.getString("messages.enabled");
         disabledMessage = config.getString("messages.disabled");
         noPermissionMessage = config.getString("messages.no-permission");
         hitmaxusageMessage = config.getString("messages.hitmaxusage");
+        hitmaxblockMessage = config.getString("messages.hitmaxblock");
         maxUsesPerDay = config.getInt("max-uses-per-day");
+        maxBlocksPerDay = config.getInt("max-blocks-per-day");
     }
 
     @Override
@@ -98,23 +140,29 @@ public class AutoTreeChop extends JavaPlugin implements Listener, CommandExecuto
         UUID playerUUID = player.getUniqueId();
         PlayerConfig playerConfig = getPlayerConfig(playerUUID);
 
-        if (playerConfig.isAutoTreeChopEnabled()) {
-            Block block = event.getBlock();
-            Material material = block.getType();
+        Block block = event.getBlock();
+        Material material = block.getType();
 
-            if (isLog(material)) {
+        if (playerConfig.isAutoTreeChopEnabled() && isLog(material)) {
+
+            if (!player.hasPermission("autotreechop.vip") && playerConfig.getDailyBlocksBroken() >= maxBlocksPerDay) {
+                player.sendMessage(hitmaxblockMessage);
                 event.setCancelled(true);
-                checkedLocations.clear();
-                chopTree(block);
-
-                if (player.getInventory().firstEmpty() == -1) {
-                    player.getWorld().dropItem(player.getLocation(), new ItemStack(material));
-                } else {
-                    player.getInventory().addItem(new ItemStack(material));
-                }
-
-                playerConfig.incrementDailyUses();
+                return;
             }
+
+            event.setCancelled(true);
+            checkedLocations.clear();
+            chopTree(block);
+
+            if (player.getInventory().firstEmpty() == -1) {
+                player.getWorld().dropItem(player.getLocation(), new ItemStack(material));
+            } else {
+                player.getInventory().addItem(new ItemStack(material));
+            }
+
+            playerConfig.incrementDailyUses();
+            playerConfig.incrementDailyBlocksBroken();
         }
     }
 
@@ -172,6 +220,7 @@ public class AutoTreeChop extends JavaPlugin implements Listener, CommandExecuto
 
         private boolean autoTreeChopEnabled;
         private int dailyUses;
+        private int dailyBlocksBroken;
         private LocalDate lastUseDate;
 
         public PlayerConfig(UUID playerUUID) {
@@ -179,6 +228,7 @@ public class AutoTreeChop extends JavaPlugin implements Listener, CommandExecuto
             this.config = YamlConfiguration.loadConfiguration(configFile);
             this.autoTreeChopEnabled = false;
             this.dailyUses = 0;
+            this.dailyBlocksBroken = 0;
             this.lastUseDate = LocalDate.now();
             loadConfig();
             saveConfig();
@@ -188,10 +238,12 @@ public class AutoTreeChop extends JavaPlugin implements Listener, CommandExecuto
             if (configFile.exists()) {
                 autoTreeChopEnabled = config.getBoolean("autoTreeChopEnabled");
                 dailyUses = config.getInt("dailyUses");
+                dailyBlocksBroken = config.getInt("dailyBlocksBroken", 0);
                 lastUseDate = LocalDate.parse(Objects.requireNonNull(config.getString("lastUseDate")));
             } else {
                 config.set("autoTreeChopEnabled", autoTreeChopEnabled);
                 config.set("dailyUses", dailyUses);
+                config.set("dailyBlocksBroken", dailyBlocksBroken);
                 String lastUseDateString = config.getString("lastUseDate");
                 if (lastUseDateString != null) {
                     lastUseDate = LocalDate.parse(lastUseDateString);
@@ -242,5 +294,26 @@ public class AutoTreeChop extends JavaPlugin implements Listener, CommandExecuto
             config.set("dailyUses", dailyUses);
             saveConfig();
         }
+        public int getDailyBlocksBroken() {
+            if (!lastUseDate.equals(LocalDate.now())) {
+                dailyBlocksBroken = 0;
+                lastUseDate = LocalDate.now();
+                config.set("dailyBlocksBroken", dailyBlocksBroken);
+                config.set("lastUseDate", lastUseDate.toString());
+                saveConfig();
+            }
+            return dailyBlocksBroken;
+        }
+
+        public void incrementDailyBlocksBroken() {
+            if (!lastUseDate.equals(LocalDate.now())) {
+                dailyBlocksBroken = 0;
+                lastUseDate = LocalDate.now();
+            }
+            dailyBlocksBroken++;
+            config.set("dailyBlocksBroken", dailyBlocksBroken);
+            saveConfig();
+        }
+
     }
 }
