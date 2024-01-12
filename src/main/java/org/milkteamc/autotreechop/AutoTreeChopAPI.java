45d6021ac0d3ae0b0d85fe6172c0ce3d6600e029
Maoyue <95519633+MagicTeaMC@users.noreply.github.com>
2024-01-13 07:06:54 +0800

    add API usage

--- src/main/java/org/milkteamc/autotreechop/AutoTreeChopAPI.java
+++ src/main/java/org/milkteamc/autotreechop/AutoTreeChopAPI.java
@@ -12,25 +12,48 @@ public class AutoTreeChopAPI {
         this.plugin = plugin;
     }
 
+    /**
+     * Get if AutoTreeChop is enabled
+     * @param player
+     * @return boolean
+     */
     public boolean isAutoTreeChopEnabled(Player player) {
         PlayerConfig playerConfig = plugin.getPlayerConfig(player.getUniqueId());
         return playerConfig.isAutoTreeChopEnabled();
     }
 
+    /**
+     * Set specific player AutoTreeChop as enabled
+     * @param player
+     */
     public void enableAutoTreeChop(Player player) {
         PlayerConfig playerConfig = plugin.getPlayerConfig(player.getUniqueId());
         playerConfig.setAutoTreeChopEnabled(true);
     }
 
+    /**
+     * Set specific player AutoTreeChop as disable
+     * @param player
+     */
     public void disableAutoTreeChop(Player player) {
         PlayerConfig playerConfig = plugin.getPlayerConfig(player.getUniqueId());
         playerConfig.setAutoTreeChopEnabled(false);
     }
 
+    /**
+     * Get how many times player use AutoTreeChop today
+     * @param playerUUID
+     * @return int
+     */
     public int getPlayerDailyUses(UUID playerUUID) {
         return plugin.getPlayerDailyUses(playerUUID);
     }
 
+    /**
+     * Get how many blocks player break via AutoTreeChop today
+     * @param playerUUID
+     * @return int
+     */
     public int getPlayerDailyBlocksBroken(UUID playerUUID) {
         return plugin.getPlayerDailyBlocksBroken(playerUUID);
     }
