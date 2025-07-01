f19887e9ee63287dcd6e0e20e4f65bfb39669252
Lee Ewing <mrlee588@gmail.com>
2025-07-01 16:05:43 -0500

    Updated dependencies and code to 1.21.6

--- src/main/java/org/milkteamc/autotreechop/utils/EffectUtils.java
+++ src/main/java/org/milkteamc/autotreechop/utils/EffectUtils.java
@@ -13,11 +13,11 @@ public class EffectUtils {
     // Sends a message to the player and shows a red particle effect indicating the block limit has been reached
     public static void sendMaxBlockLimitReachedMessage(Player player, Block block, Message HIT_MAX_BLOCK_MESSAGE) {
         sendMessage(player, HIT_MAX_BLOCK_MESSAGE);
-        player.getWorld().spawnParticle(Particle.REDSTONE, block.getLocation().add(0.5, 0.5, 0.5), 50, 0.5, 0.5, 0.5, 0, new Particle.DustOptions(Color.RED, 1));
+        player.getWorld().spawnParticle(Particle.DUST, block.getLocation().add(0.5, 0.5, 0.5), 50, 0.5, 0.5, 0.5, 0, new Particle.DustOptions(Color.RED, 1));
     }
 
     // Shows a green particle effect indicating the block has been chopped
     public static void showChopEffect(Player player, Block block) {
-        player.getWorld().spawnParticle(Particle.VILLAGER_HAPPY, block.getLocation().add(0.5, 0.5, 0.5), 50, 0.5, 0.5, 0.5, 0);
+        player.getWorld().spawnParticle(Particle.HAPPY_VILLAGER, block.getLocation().add(0.5, 0.5, 0.5), 50, 0.5, 0.5, 0.5, 0);
     }
 }
