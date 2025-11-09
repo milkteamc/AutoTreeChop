7a849a08660ae83768cf3c7b75b18b06d54a8021
MagicTeaMC <oneowo87@gmail.com>
2025-11-09 16:38:59 +0800

    [ci skip] cleanup, stark working on 1.7.2

--- src/main/java/org/milkteamc/autotreechop/utils/ProtectionCheckUtils.java
+++ src/main/java/org/milkteamc/autotreechop/utils/ProtectionCheckUtils.java
@@ -9,39 +9,12 @@ import org.milkteamc.autotreechop.hooks.WorldGuardHook;
 
 public class ProtectionCheckUtils {
 
-    public static class ProtectionHooks {
-        public final boolean worldGuardEnabled;
-        public final boolean residenceEnabled;
-        public final boolean griefPreventionEnabled;
-        public final boolean landsEnabled;
-
-        public final WorldGuardHook worldGuard;
-        public final ResidenceHook residence;
-        public final GriefPreventionHook griefPrevention;
-        public final LandsHook lands;
-
-        public ProtectionHooks(
-                boolean worldGuardEnabled, WorldGuardHook worldGuard,
-                boolean residenceEnabled, ResidenceHook residence,
-                boolean griefPreventionEnabled, GriefPreventionHook griefPrevention,
-                boolean landsEnabled, LandsHook lands) {
-            this.worldGuardEnabled = worldGuardEnabled;
-            this.worldGuard = worldGuard;
-            this.residenceEnabled = residenceEnabled;
-            this.residence = residence;
-            this.griefPreventionEnabled = griefPreventionEnabled;
-            this.griefPrevention = griefPrevention;
-            this.landsEnabled = landsEnabled;
-            this.lands = lands;
-        }
-    }
-
     /**
      * Unified permission check.
      *
-     * @param player Player to check
+     * @param player   Player to check
      * @param location Location to check
-     * @param hooks Protection hooks container
+     * @param hooks    Protection hooks container
      * @return true if player can build at location, false otherwise
      */
     public static boolean canModifyBlock(Player player, Location location, ProtectionHooks hooks) {
@@ -77,9 +50,9 @@ public class ProtectionCheckUtils {
      * Batch permission check for multiple locations.
      * Uses early-exit optimization - stops checking once first failure is found.
      *
-     * @param player Player to check
+     * @param player    Player to check
      * @param locations Locations to check
-     * @param hooks Protection hooks container
+     * @param hooks     Protection hooks container
      * @return true if player can build at ALL locations, false otherwise
      */
     public static boolean canModifyAllBlocks(Player player, Iterable<Location> locations, ProtectionHooks hooks) {
@@ -109,5 +82,32 @@ public class ProtectionCheckUtils {
 
     public static boolean checkLands(Player player, Location location, boolean enabled, LandsHook hook) {
         return !enabled || hook.checkBuild(player, location);
+    }
+
+    public static class ProtectionHooks {
+        public final boolean worldGuardEnabled;
+        public final boolean residenceEnabled;
+        public final boolean griefPreventionEnabled;
+        public final boolean landsEnabled;
+
+        public final WorldGuardHook worldGuard;
+        public final ResidenceHook residence;
+        public final GriefPreventionHook griefPrevention;
+        public final LandsHook lands;
+
+        public ProtectionHooks(
+                boolean worldGuardEnabled, WorldGuardHook worldGuard,
+                boolean residenceEnabled, ResidenceHook residence,
+                boolean griefPreventionEnabled, GriefPreventionHook griefPrevention,
+                boolean landsEnabled, LandsHook lands) {
+            this.worldGuardEnabled = worldGuardEnabled;
+            this.worldGuard = worldGuard;
+            this.residenceEnabled = residenceEnabled;
+            this.residence = residence;
+            this.griefPreventionEnabled = griefPreventionEnabled;
+            this.griefPrevention = griefPrevention;
+            this.landsEnabled = landsEnabled;
+            this.lands = lands;
+        }
     }
 }
